f469c13bfc4a9de591cbbc596f0866056e16d405
Saumya Zeron <bhartisaumya22@gmail.com>
2024-11-05 20:07:39 +0530

    implemented get and insert note features

--- src/databases.ts
+++ src/databases.ts
@@ -1,27 +1,41 @@
-// import {createPool} from 'mysql2'
+import {createPool} from 'mysql2'
 
 
-// const pool = createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '6862',
-//     database: 'notes_app'
-// }).promise()
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '6862',
+    database: 'notes_app'
+}).promise()
 
-
-// async function getNotes(){
-//     const result = await pool.query("SELECT *FROM notes")
-//     return result
-// }
-
-// const ans = getNotes()
-// console.log(ans.then(result => console.log(result[0])))
-
-
-
-let c = 0;
-
-while(1){
-    c++;
-    console.log(c)
+interface INotes {
+    id: number,
+    title: string,
+    contents: string,
+    created_at: string
 }
+
+
+export const getNotes = async (id: number): Promise<undefined | INotes> => {
+    try {
+        const [result]: any  = await pool.query(`SELECT *FROM notes WHERE id = ?`, [id])
+        return result[0];
+        
+    } catch (error) {
+        console.log("Error:",error)
+    }   
+}
+
+export const insertNotes = async(title: string, contents: string) => {
+    try {
+        const [result]: any = await pool.query(`INSERT INTO notes (title, contents) VALUES (?, ?)`, [title, contents])
+        return getNotes(result.insertId)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import express from 'express'
 
-const app = express()
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
 
-import './databases'
+import {getNotes, insertNotes} from './databases'
+
+app.get('/:id', async(req, res) => {
+    try {
+        const id: number = parseInt(req.params.id)
+
+        const result = await getNotes(id)
+
+        res.status(201).json(result)
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+
+app.post('/', async(req, res) => {
+    try {
+        const {title, contents} = req.body
+
+
+        console.log(contents)
+
+        const result = await insertNotes(title, contents)
+
+        res.status(201).json(result)
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
 
 app.listen(8080)
